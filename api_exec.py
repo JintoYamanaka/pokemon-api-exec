ed586c59ed817381ed4c2956ad4d93c0e1174a4b
JintoYamanaka <yamajin.ips11@gmail.com>
2023-11-05 01:41:34 +0900

    リファクタ

--- api_exec.py
+++ api_exec.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import json
 import time
@@ -5,43 +6,50 @@ import time
 # APIのベースURL
 BASE_URL = "https://pokeapi.co/api/v2/"
 
-# エンドポイントとそのクエリパラメータ
-endpoints_with_params = {
-    'ability': '1',  # IDで指定
-    'characteristic': '1',  # IDで指定
-    'egg-group': '1',  # IDで指定
-    'gender': '1',  # IDで指定
-    'growth-rate': '1',  # IDで指定
-    'nature': '1',  # IDで指定
-    'pokeathlon-stat': '1',  # IDで指定
-    'pokemon': '1',  # IDで指定、"bulbasaur"などの名前でもOK
-    'pokemon-color': '1',  # IDで指定
-    'pokemon-form': '1',  # IDで指定
-    'pokemon-habitat': '1',  # IDで指定
-    'pokemon-shape': '1',  # IDで指定
-    'pokemon-species': '1',  # IDで指定
-    'stat': '1',  # IDで指定
-    'type': '1',  # IDで指定
-}
+# エンドポイント
+endpoints = [
+    'ability', 'characteristic', 'egg-group', 'gender', 'growth-rate',
+    'nature', 'pokeathlon-stat', 'pokemon', 'pokemon-color', 'pokemon-form',
+    'pokemon-habitat', 'pokemon-shape', 'pokemon-species', 'stat', 'type'
+]
 
-# データ取得とファイル保存の関数
-def fetch_and_save_data(endpoint, param):
+# JSONを保存するディレクトリ
+JSON_DIR = 'result'
+
+# ディレクトリが存在しない場合は作成
+os.makedirs(JSON_DIR, exist_ok=True)
+
+# データ取得関数
+def fetch_data(endpoint, param):
     url = f"{BASE_URL}{endpoint}/{param}/"
-    response = requests.get(url)
-    if response.ok:
-        data = response.json()
-        filename = f"{endpoint}_{param}.json"
-        with open(filename, 'w') as file:
-            json.dump(data, file, indent=2)
-        print(f"Saved {filename}")
-    else:
-        print(f"Failed to retrieve data for {endpoint} with param {param}")
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.HTTPError as errh:
+        print(f"Http Error: {errh}")
+    except requests.exceptions.ConnectionError as errc:
+        print(f"Error Connecting: {errc}")
+    except requests.exceptions.Timeout as errt:
+        print(f"Timeout Error: {errt}")
+    except requests.exceptions.RequestException as err:
+        print(f"OOps: Something Else: {err}")
+
+# データ保存関数
+def save_data(data, filename):
+    file_path = os.path.join(JSON_DIR, filename)
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=2)
+    print(f"Saved {file_path}")
 
 # メイン処理
 def main():
-    for endpoint, param in endpoints_with_params.items():
-        fetch_and_save_data(endpoint, param)
-        time.sleep(1)  # APIのレートリミットに配慮して1秒待機
+    for endpoint in endpoints:
+        data = fetch_data(endpoint, '1')
+        if data:
+            filename = f"{endpoint}_1.json"
+            save_data(data, filename)
+            time.sleep(1)  # APIのレートリミットに配慮
 
 if __name__ == "__main__":
     main()
